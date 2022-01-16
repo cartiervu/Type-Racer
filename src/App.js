8256914516ee9656cd9e6cfa09d7fec2c5b26c00
Cartier Vu <vu.cartier@gmail.com>
2022-01-15 20:11:58 -0800

    Made input field hidden

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import DisplayQuote from './components/DisplayQuote'
 
+// TODO: Focus on input field
 const App = () => {
   const [userText, setUserText] = useState('');
   const quote = "Even the world's most successful individuals have experienced their fair share of setbacks and hardships. There's much to learn from their challenges as well as their success. Luckily, they've condensed their wisdom into meaningful quotes that you can store for later use."
 
   const handleUserTextChange = (event) => {
-    event.preventDefault();
     if (event.key === 'Backspace') {
       setUserText(userText.substring(0, userText.length - 1));
     } else if (event.key === quote.charAt(userText.length)) {
@@ -16,16 +16,13 @@ const App = () => {
   }
   
   return (
-    <div>
+    <div class="text-container">
       <DisplayQuote cName='one' quote={quote.substring(0,userText.length)} />
       <DisplayQuote cName='two' quote={quote.substring(userText.length,quote.length)} />
       <p></p>
-      <form>
-        <input 
-          value={userText}
-          onKeyDown={(e) => handleUserTextChange(e)}
-        />
-      </form>
+      <div id="text-box">
+        <input onKeyDown={(e) => handleUserTextChange(e)} autoFocus />
+      </div>
     </div>
   )
 }
