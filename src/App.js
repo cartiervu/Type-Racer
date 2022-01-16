bdb040ad7d16d48b476a9b485e2656bd09b6f752
Cartier Vu <vu.cartier@gmail.com>
2022-01-15 18:15:29 -0800

    Prevent default for form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const quote = "Even the world's most successful individuals have experienced their fair share of setbacks and hardships. There's much to learn from their challenges as well as their success. Luckily, they've condensed their wisdom into meaningful quotes that you can store for later use."
 
   const handleUserTextChange = (event) => {
-    console.log(event.key);
+    event.preventDefault();
     if (event.key === 'Backspace') {
       setUserText(userText.substring(0, userText.length - 1));
     } else if (event.key === quote.charAt(userText.length)) {
