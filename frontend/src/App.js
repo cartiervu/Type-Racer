9ce3246f0b7be23ecb66a5ba0d5d5adda180076f
Cartier Vu <vu.cartier@gmail.com>
2022-02-06 15:00:33 -0800

    moved files to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Text from './components/UserText';
 import DisplayResults from './components/DisplayResults'
 
 async function getRandomWikiPage() {
-  const endpoint = `https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&generator=random&grnnamespace=0&prop=revisions%7Cimages&rvprop=content&grnlimit=25`
+  const endpoint = `https://simple.wikipedia.org/w/api.php?format=json&origin=*&action=query&generator=random&grnnamespace=0&prop=revisions%7Cimages&rvprop=content&grnlimit=25`
   const response = await fetch(endpoint);
   
   if (!response.ok) {
@@ -18,14 +18,15 @@ async function getRandomWikiPage() {
       result = result.replace(/[^\x00-\x7F]/g, "");
       result = result.replace("  ", " ")
       
-      if (result.length > 25 && result.length < 100) {
+      if (result.length > 25 && result.length < 250) {
           return result;
       }
+      // return result;
   }
 }
 
 async function searchWikipedia(id) {
-  const endpoint = `https://en.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${id}`;
+  const endpoint = `https://simple.wikipedia.org/w/api.php?format=json&origin=*&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=${id}`;
   const response = await fetch(endpoint);
   if (!response.ok) {
       throw Error(response.statusText);
@@ -35,27 +36,17 @@ async function searchWikipedia(id) {
   return json.query.pages[id].extract;
 }
 
-const App = () => {
+export default function App({ scores }) {
   const [quote, setQuote] = useState('');
-
-
-  (async () => {
-    const result = await getRandomWikiPage();
-    if (quote === '') {
-      setQuote(result)
-    }
-  })()  
-
-  return (
-    <Main quote={quote}/>
-  )
-}
-
-const Main = ({quote}) => {
   const [timer, setTimer] = useState({
     startTime: null,
     endTime: null
   });
+
+  useEffect(() => {
+    getRandomWikiPage()
+      .then(response => setQuote(response))
+  }, []);
 
   const handleOnFinish = () => {
     const newTimer = {
@@ -70,10 +61,8 @@ const Main = ({quote}) => {
       {!timer.endTime ? (
         <Text quote={quote} timer={timer} onFinish={(handleOnFinish)}/>
         ) : (
-          <DisplayResults quote={quote} timer={timer}/>
+          <DisplayResults scores={scores} quote={quote} timer={timer}/>
         )}
     </div>
   )
 }
-
-export default App
---
+++ frontend/src/components/DisplayResults.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function computeWPM(quote, timer) {
+  const words = (quote.split(' ')).length;
+  const timeElapsed = (timer.endTime - timer.startTime) / 60000;
+
+  return (words / timeElapsed).toFixed(1);
+}
+
+
+const DisplayLeaderboard = ({ scores }) => {
+  return (
+    <div className='leaderboard'>
+      <div className='leaderboard-header'>
+        <div className='username'>Name</div>
+        <div className='wpm'>WPM</div>
+      </div>
+      {scores.map(score => (
+        <div key={score.id}>
+          <div className='player-score-container'>
+            <div className='username'>{score.username}</div>
+            <div className='wpm'>{score.wpm}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default function DisplayResults({ scores, quote, timer }) {
+  return (
+    <div className='results'>
+      <DisplayLeaderboard scores={scores} />
+      <hr/>
+      <div className='wpm'>
+        Words Per Minute: {computeWPM(quote, timer)}
+      </div>
+    </div>
+  )
+}
